2d44b8e987eebbbf8d171497570cc6aca5768074
Fengenst <1910750670@qq.com>
2025-08-31 14:28:59 +0800

    feature: 开发短链接后管系统查询用户信息脱敏功能

--- admin/src/main/java/com/tenseed/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/tenseed/shortlink/admin/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.tenseed.shortlink.admin.controller;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.tenseed.shortlink.admin.common.convention.result.Result;
 import com.tenseed.shortlink.admin.common.convention.result.Results;
+import com.tenseed.shortlink.admin.dto.resp.UserActualRespDTO;
 import com.tenseed.shortlink.admin.dto.resp.UserRespDTO;
 import com.tenseed.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,10 +22,18 @@ public class UserController {
     private final UserService userService;
 
     /**
-     * 根据用户名查找用户信息
+     * 根据用户名查找脱敏用户信息
      */
     @GetMapping("/api/shortlink/v1/user/{username}")
     public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username) {
         return Results.success(userService.getUserByUsername(username));
+    }
+
+    /**
+     * 根据用户名查找无脱敏用户信息
+     */
+    @GetMapping("/api/shortlink/v1/actual/user/{username}")
+    public Result<UserActualRespDTO> getActualUserByUsername(@PathVariable("username") String username) {
+        return Results.success(BeanUtil.toBean(userService.getUserByUsername(username), UserActualRespDTO.class));
     }
 }
