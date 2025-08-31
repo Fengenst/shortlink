fc8c06c437054fb08e6199fbb25b6f97a407d7e2
Fengenst <1910750670@qq.com>
2025-08-31 12:38:31 +0800

    feature: 开发短链接规约功能

---
+++ admin/src/main/java/com/tenseed/shortlink/admin/common/convention/exception/RemoteException.java
@@ -0,0 +1,30 @@
+package com.tenseed.shortlink.admin.common.convention.exception;
+
+import com.tenseed.shortlink.admin.common.convention.errorcode.IErrorCode;
+import com.tenseed.shortlink.admin.common.convention.errorcode.BaseErrorCode;
+
+/**
+ * 远程服务调用异常
+ */
+public class RemoteException extends AbstractException {
+
+    public RemoteException(String message) {
+        this(message, null, BaseErrorCode.REMOTE_ERROR);
+    }
+
+    public RemoteException(String message, IErrorCode errorCode) {
+        this(message, null, errorCode);
+    }
+
+    public RemoteException(String message, Throwable throwable, IErrorCode errorCode) {
+        super(message, throwable, errorCode);
+    }
+
+    @Override
+    public String toString() {
+        return "RemoteException{" +
+                "code='" + errorCode + "'," +
+                "message='" + errorMessage + "'" +
+                '}';
+    }
+}
