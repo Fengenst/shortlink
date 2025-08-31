caed1a35e9f28e0e6d1fba4c6de1a4816371f197
Fengenst <1910750670@qq.com>
2025-08-31 12:41:17 +0800

    feature: 开发短链接后管系统相关功能

---
+++ admin/src/main/java/com/tenseed/shortlink/admin/service/UserService.java
@@ -0,0 +1,18 @@
+package com.tenseed.shortlink.admin.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.tenseed.shortlink.admin.dao.entity.UserDO;
+import com.tenseed.shortlink.admin.dto.resp.UserRespDTO;
+
+/**
+ * 用户接口层
+ */
+public interface UserService extends IService<UserDO> {
+    /**
+     * 根据用户名查询用户信息
+     *
+     * @param username 用户名
+     * @return 用户返回实体
+     */
+    UserRespDTO getUserByUsername(String username);
+}
