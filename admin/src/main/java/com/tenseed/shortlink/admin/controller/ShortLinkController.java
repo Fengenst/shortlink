db91bd0565b6915bcec0616a1e30eb67aff25e96
Fengenst <1910750670@qq.com>
2025-09-10 21:17:01 +0800

    feature: 短链接后管系统调用中台请求相关接口

---
+++ admin/src/main/java/com/tenseed/shortlink/admin/controller/ShortLinkController.java
@@ -0,0 +1,40 @@
+package com.tenseed.shortlink.admin.controller;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.tenseed.shortlink.admin.common.convention.result.Result;
+import com.tenseed.shortlink.admin.remote.dto.ShortLinkRemoteService;
+import com.tenseed.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
+import com.tenseed.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
+import com.tenseed.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
+import com.tenseed.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 短链接后管控制层
+ */
+@RestController
+public class ShortLinkController {
+
+    // TODO 后续重构为 SpringCloud Feign 调用
+    ShortLinkRemoteService shortLinkRemoteService = new ShortLinkRemoteService() {
+    };
+
+    /**
+     * 短链接创建
+     */
+    @PostMapping("/api/shortlink/admin/v1/create")
+    public Result<ShortLinkCreateRespDTO> createShortLink(@RequestBody ShortLinkCreateReqDTO requestParam) {
+        return shortLinkRemoteService.createShortLink(requestParam);
+    }
+
+    /**
+     * 短链接分页查询
+     */
+    @GetMapping("/api/shortlink/admin/v1/page")
+    public Result<IPage<ShortLinkPageRespDTO>> pageShortLink(ShortLinkPageReqDTO requestParam) {
+        return shortLinkRemoteService.pageShortLink(requestParam);
+    }
+}
---
+++ admin/src/main/java/com/tenseed/shortlink/admin/remote/dto/ShortLinkRemoteService.java
@@ -0,0 +1,48 @@
+package com.tenseed.shortlink.admin.remote.dto;
+
+import cn.hutool.http.HttpUtil;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.TypeReference;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.tenseed.shortlink.admin.common.convention.result.Result;
+import com.tenseed.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
+import com.tenseed.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
+import com.tenseed.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
+import com.tenseed.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
+
+import java.util.HashMap;
+
+/**
+ * 短链接中台远程调用服务
+ */
+public interface ShortLinkRemoteService {
+
+    /**
+     * 创建短链接
+     *
+     * @param requestParam 创建短链接请求参数
+     * @return 短链接创建响应
+     */
+    default Result<ShortLinkCreateRespDTO> createShortLink(ShortLinkCreateReqDTO requestParam) {
+        String resultBodyStr = HttpUtil.post("http://127.0.0.1:8004/api/shortlink/v1/create", JSON.toJSONString(requestParam));
+        return JSON.parseObject(resultBodyStr, new TypeReference<>() {
+        });
+    }
+
+    /**
+     * 分页查询短链接
+     *
+     * @param requestParam 分页短链接请求参数
+     * @return 分页短链接请求响应
+     */
+    default Result<IPage<ShortLinkPageRespDTO>> pageShortLink(ShortLinkPageReqDTO requestParam) {
+        HashMap<String, Object> requestMap = new HashMap<>();
+        requestMap.put("gid", requestParam.getGid());
+        requestMap.put("current", requestParam.getCurrent());
+        requestMap.put("size", requestParam.getSize());
+        String resultPageStr = HttpUtil.get("http://127.0.0.1:8004/api/shortlink/v1/page", requestMap);
+        return JSON.parseObject(resultPageStr, new TypeReference<>() {
+        });
+    }
+
+}
