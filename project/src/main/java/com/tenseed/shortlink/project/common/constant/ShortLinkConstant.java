28a398e927ce73955493eba5134f5f01d4c173b3
Fengenst <1910750670@qq.com>
2025-09-15 20:24:41 +0800

    optimize: 优化短链接常量类

--- project/src/main/java/com/tenseed/shortlink/project/common/constant/ShortLinkConstant.java
+++ project/src/main/java/com/tenseed/shortlink/project/common/constant/ShortLinkConstant.java
@@ -6,7 +6,7 @@ package com.tenseed.shortlink.project.common.constant;
 public class ShortLinkConstant {
 
     /**
-     * 永久短链接默认缓存时间
+     * 永久短链接默认缓存时间，默认一个月
      */
     public static final long DEFAULT_CACHE_VALID_DATE = 2592000000L;
 }
