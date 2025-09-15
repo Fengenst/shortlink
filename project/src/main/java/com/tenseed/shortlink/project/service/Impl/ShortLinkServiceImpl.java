110028c7d4da1e451bc399e9745bf1501d8e8786
Fengenst <1910750670@qq.com>
2025-09-15 20:20:14 +0800

    optimize: 短链接创建时进行缓存预热

--- project/src/main/java/com/tenseed/shortlink/project/service/Impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/tenseed/shortlink/project/service/Impl/ShortLinkServiceImpl.java
@@ -23,6 +23,7 @@ import com.tenseed.shortlink.project.dto.resp.ShortLinkGroupCountQueryRespDTO;
 import com.tenseed.shortlink.project.dto.resp.ShortLinkPageRespDTO;
 import com.tenseed.shortlink.project.service.ShortLinkService;
 import com.tenseed.shortlink.project.toolkit.HashUtil;
+import com.tenseed.shortlink.project.toolkit.LinkUtil;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletResponse;
@@ -102,7 +103,12 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                 throw new ServiceException("短链接生成重复");
             }
         }
-
+        // 短链接创建时进行缓存预热
+        stringRedisTemplate.opsForValue().set(
+                String.format(GOTO_SHORT_LINK_KEY, fullShortUrl),
+                requestParam.getOriginUrl(),
+                LinkUtil.getLinkCacheValidTime(requestParam.getValidDate()), TimeUnit.MILLISECONDS
+        );
         shortUriCreateCachePenetrationBloomFilter.add(fullShortUrl);
         return ShortLinkCreateRespDTO.builder()
                 .fullShortUrl("http://" + shortLinkDO.getFullShortUrl())
---
+++ project/src/main/java/com/tenseed/shortlink/project/toolkit/LinkUtil.java
@@ -0,0 +1,27 @@
+package com.tenseed.shortlink.project.toolkit;
+
+import cn.hutool.core.date.DateUnit;
+import cn.hutool.core.date.DateUtil;
+
+import java.util.Date;
+import java.util.Optional;
+
+import static com.tenseed.shortlink.project.common.constant.ShortLinkConstant.DEFAULT_CACHE_VALID_DATE;
+
+/**
+ * 短链接工具类
+ */
+public class LinkUtil {
+
+    /**
+     * 获取短链接缓存有效期时间
+     *
+     * @param validDate 有效期时间
+     * @return 有限期时间戳
+     */
+    public static Long getLinkCacheValidTime(Date validDate) {
+        return Optional.ofNullable(validDate)
+                .map(each -> DateUtil.between(new Date(), each, DateUnit.MS))
+                .orElse(DEFAULT_CACHE_VALID_DATE);
+    }
+}
