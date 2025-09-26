6e78736a64d25dbcb5a170bfb45ee267e9e0e6af
Fengenst <1910750670@qq.com>
2025-09-26 13:16:03 +0800

    optimize: 使用文本块优化 sql 语句

---
+++ project/src/main/java/com/tenseed/shortlink/project/dao/mapper/LinkLocaleStatsMapper.java
@@ -0,0 +1,10 @@
+package com.tenseed.shortlink.project.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.tenseed.shortlink.project.dao.entity.LinkLocaleStatsDO;
+
+/**
+ * 地区访问统计持久层
+ */
+public interface LinkLocaleStatsMapper extends BaseMapper<LinkLocaleStatsDO> {
+}
