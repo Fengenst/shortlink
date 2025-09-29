85407b44e98db2cf17b5e4eedd127e5bce397478
Fengenst <1910750670@qq.com>
2025-09-29 20:16:32 +0800

    feature: 开发分页查询短链接访问日志

--- project/src/main/java/com/tenseed/shortlink/project/service/ShortLinkStatsService.java
+++ project/src/main/java/com/tenseed/shortlink/project/service/ShortLinkStatsService.java
@@ -1,8 +1,14 @@
 package com.tenseed.shortlink.project.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.tenseed.shortlink.project.dto.req.ShortLinkStatsAccessRecordReqDTO;
 import com.tenseed.shortlink.project.dto.req.ShortLinkStatsReqDTO;
+import com.tenseed.shortlink.project.dto.resp.ShortLinkStatsAccessRecordRespDTO;
 import com.tenseed.shortlink.project.dto.resp.ShortLinkStatsRespDTO;
 
+/**
+ * 短链接监控接口
+ */
 public interface ShortLinkStatsService {
 
     /**
@@ -12,4 +18,12 @@ public interface ShortLinkStatsService {
      * @return 短链接监控数据
      */
     ShortLinkStatsRespDTO oneShortLinkStats(ShortLinkStatsReqDTO requestParam);
+
+    /**
+     * 访问单个短链接指定时间内访问记录监控数据
+     *
+     * @param requestParam 获取短链接监控访问记录数据入参
+     * @return 访问记录监控数据
+     */
+    IPage<ShortLinkStatsAccessRecordRespDTO> shortLinkStatsAccessRecord(ShortLinkStatsAccessRecordReqDTO requestParam);
 }
