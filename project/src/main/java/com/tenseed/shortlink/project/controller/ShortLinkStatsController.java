da74ab8850938c09ad46e7a39d1ed5c1124756d9
Fengenst <1910750670@qq.com>
2025-09-29 20:23:49 +0800

    Reapply "feature: 开发分页查询短链接访问日志"
    This reverts commit 7cc8039a5ca6924842e8dec544c594ce0b2d1b6a.

--- project/src/main/java/com/tenseed/shortlink/project/controller/ShortLinkStatsController.java
+++ project/src/main/java/com/tenseed/shortlink/project/controller/ShortLinkStatsController.java
@@ -1,14 +1,20 @@
 package com.tenseed.shortlink.project.controller;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.tenseed.shortlink.project.common.convention.result.Result;
 import com.tenseed.shortlink.project.common.convention.result.Results;
+import com.tenseed.shortlink.project.dto.req.ShortLinkStatsAccessRecordReqDTO;
 import com.tenseed.shortlink.project.dto.req.ShortLinkStatsReqDTO;
+import com.tenseed.shortlink.project.dto.resp.ShortLinkStatsAccessRecordRespDTO;
 import com.tenseed.shortlink.project.dto.resp.ShortLinkStatsRespDTO;
 import com.tenseed.shortlink.project.service.ShortLinkStatsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 短链接监控控制层
+ */
 @RestController
 @RequiredArgsConstructor
 public class ShortLinkStatsController {
@@ -21,5 +27,13 @@ public class ShortLinkStatsController {
     @GetMapping("/api/short-link/v1/stats")
     public Result<ShortLinkStatsRespDTO> shortLinkStats(ShortLinkStatsReqDTO requestParam) {
         return Results.success(shortLinkStatsService.oneShortLinkStats(requestParam));
+    }
+
+    /**
+     * 访问单个短链接指定时间内访问记录监控数据
+     */
+    @GetMapping("/api/short-link/v1/stats/access-record")
+    public Result<IPage<ShortLinkStatsAccessRecordRespDTO>> shortLinkStats(ShortLinkStatsAccessRecordReqDTO requestParam) {
+        return Results.success(shortLinkStatsService.shortLinkStatsAccessRecord(requestParam));
     }
 }
