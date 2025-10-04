1353bcce57d10572a891ddff38c86e0f74ffc47a
Fengenst <1910750670@qq.com>
2025-10-04 14:31:44 +0800

    optimize: 优化单个用户创建分组数量不能大于20

--- admin/src/main/java/com/tenseed/shortlink/admin/service/Impl/GroupServiceImpl.java
+++ admin/src/main/java/com/tenseed/shortlink/admin/service/Impl/GroupServiceImpl.java
@@ -1,12 +1,14 @@
 package com.tenseed.shortlink.admin.service.Impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.tenseed.shortlink.admin.common.biz.user.UserContext;
+import com.tenseed.shortlink.admin.common.convention.exception.ClientException;
 import com.tenseed.shortlink.admin.common.convention.result.Result;
 import com.tenseed.shortlink.admin.dao.entity.GroupDO;
 import com.tenseed.shortlink.admin.dao.mapper.GroupMapper;
@@ -18,10 +20,15 @@ import com.tenseed.shortlink.admin.remote.dto.resp.ShortLinkGroupCountQueryRespD
 import com.tenseed.shortlink.admin.service.GroupService;
 import com.tenseed.shortlink.admin.toolkit.RandomGenerator;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
 import java.util.stream.Collectors;
+
+import static com.tenseed.shortlink.admin.common.constant.RedisCacheConstant.LOCK_GROUP_CREATE_KEY;
 
 /**
  * 短链接分组接口实现层
@@ -30,9 +37,19 @@ import java.util.stream.Collectors;
 @Service
 public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implements GroupService {
 
+    private final RedissonClient redissonClient;
+
+    @Value("${short-link.group.max-num}")
+    private Integer groupMaxNum;
+
+
     // TODO 后续重构为 SpringCloud Feign 调用
     ShortLinkRemoteService shortLinkRemoteService = new ShortLinkRemoteService() {
     };
+
+    public GroupServiceImpl(RedissonClient redissonClient) {
+        this.redissonClient = redissonClient;
+    }
 
     /**
      * 判断GID是否可用
@@ -59,20 +76,32 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
 
     @Override
     public void saveGroup(String username, String groupName) {
-        String gid;
-        // 循环生成唯一 GID，直到找到未被使用的 GID
-        do {
-            gid = RandomGenerator.generateSixAlphaNumber();
-        } while (availableGid(username, gid));
+        RLock lock = redissonClient.getLock(String.format(LOCK_GROUP_CREATE_KEY, username));
+        lock.lock();
+        try {
+            LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
+                    .eq(GroupDO::getUsername, username);
+            List<GroupDO> groupDOList = baseMapper.selectList(queryWrapper);
+            if (CollUtil.isNotEmpty(groupDOList) && groupDOList.size() == groupMaxNum) {
+                throw new ClientException(String.format("已超出最大分组数： %d", groupMaxNum));
+            }
+            String gid;
+            // 循环生成唯一 GID，直到找到未被使用的 GID
+            do {
+                gid = RandomGenerator.generateSixAlphaNumber();
+            } while (availableGid(username, gid));
 
-        // 构建分组对象并保存到数据库
-        GroupDO groupDO = GroupDO.builder()
-                .gid(gid)
-                .name(groupName)
-                .username(username)
-                .sortOrder(0)
-                .build();
-        baseMapper.insert(groupDO);
+            // 构建分组对象并保存到数据库
+            GroupDO groupDO = GroupDO.builder()
+                    .gid(gid)
+                    .name(groupName)
+                    .username(username)
+                    .sortOrder(0)
+                    .build();
+            baseMapper.insert(groupDO);
+        } finally {
+            lock.unlock();
+        }
     }
 
 
