1dc79de05fa39ec1b65dbbdf4c2b31a0bf8faf01
Fengenst <1910750670@qq.com>
2025-10-05 15:02:37 +0800

    feature: 短链接后管通过风控保障系统稳定性

--- admin/src/main/java/com/tenseed/shortlink/admin/common/config/UserConfiguration.java
+++ admin/src/main/java/com/tenseed/shortlink/admin/common/config/UserConfiguration.java
@@ -1,6 +1,8 @@
 package com.tenseed.shortlink.admin.common.config;
 
+import com.tenseed.shortlink.admin.common.biz.user.UserFlowRiskControlFilter;
 import com.tenseed.shortlink.admin.common.biz.user.UserTransmitFilter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +21,21 @@ public class UserConfiguration {
         registration.addUrlPatterns("/*");
         registration.addInitParameter("Login", "/api/shortlink/admin/v1/user/login");
         registration.setOrder(0);
+        return registration;
+    }
+
+    /**
+     * 用户操作流量风控过滤器
+     */
+    @Bean
+    @ConditionalOnProperty(name = "short-link.flow-limit.enable", havingValue = "true")
+    public FilterRegistrationBean<UserFlowRiskControlFilter> globalUserFlowRiskControlFilter(
+            StringRedisTemplate stringRedisTemplate,
+            UserFlowRiskControlConfiguration userFlowRiskControlConfiguration) {
+        FilterRegistrationBean<UserFlowRiskControlFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new UserFlowRiskControlFilter(stringRedisTemplate, userFlowRiskControlConfiguration));
+        registration.addUrlPatterns("/*");
+        registration.setOrder(10);
         return registration;
     }
 }
