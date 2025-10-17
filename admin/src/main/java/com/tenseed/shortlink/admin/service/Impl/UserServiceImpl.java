68c00aacefc8c8073d3a091a87069b48209f7d3d
Fengenst <1910750670@qq.com>
2025-10-17 20:58:59 +0800

    optimize: 修改用户信息时验证当前用户名是否为登录用户

--- admin/src/main/java/com/tenseed/shortlink/admin/service/Impl/UserServiceImpl.java
+++ admin/src/main/java/com/tenseed/shortlink/admin/service/Impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import com.alibaba.fastjson2.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.tenseed.shortlink.admin.common.biz.user.UserContext;
 import com.tenseed.shortlink.admin.common.convention.exception.ClientException;
 import com.tenseed.shortlink.admin.common.enums.UserErrorCodeEnum;
 import com.tenseed.shortlink.admin.dao.entity.UserDO;
@@ -29,6 +30,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 import static com.tenseed.shortlink.admin.common.constant.RedisCacheConstant.LOCK_USER_REGISTER_KEY;
@@ -108,7 +110,9 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
 
     @Override
     public void update(UserUpdateReqDTO requestParam) {
-        // TODO 验证当前用户名是否为登录用户
+        if (!Objects.equals(requestParam.getUsername(), UserContext.getUsername())) {
+            throw new ClientException("当前登录用户修改请求异常");
+        }
         LambdaQueryWrapper<UserDO> updateWrapper = Wrappers.lambdaQuery(UserDO.class)
                 .eq(UserDO::getUsername, requestParam.getUsername());
         baseMapper.update(BeanUtil.toBean(requestParam, UserDO.class), updateWrapper);
